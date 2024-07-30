d36de62180437a31a2a1b7fd7139cbedbe1e62fb
Totuin <117037443+Totuin@users.noreply.github.com>
2024-07-30 05:24:24 +0400

    30-07-24

---
+++ src/flprog_Can.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "flprogUtilites.h"
+
+#ifdef _STM32_DEF_
+#define FLPROG_STM32_CAN
+#else
+#define FLPROG_PLUG_CAN
+#endif
+
+#ifdef FLPROG_PLUG_CAN
+#include "flprogStm32Can.h"
+#endif
+
+#ifdef FLPROG_PLUG_CAN
+//#include "software/flprogAvrSoftRTC.h"
+#endif
+
