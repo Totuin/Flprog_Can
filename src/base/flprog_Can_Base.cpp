e99bf11f9dd64ca94e08584959f7b00a44966284
Totuin <117037443+Totuin@users.noreply.github.com>
2025-11-09 08:36:38 +0500

    08-11-25

--- src/base/flprog_Can_Base.cpp
+++ src/base/flprog_Can_Base.cpp
@@ -2,159 +2,159 @@
 
 bool FLProgAbstractCanBus::write(CAN_message_t &CAN_tx_msg, bool sendMB)
 {
-    (void)CAN_tx_msg;
-    (void)sendMB;
-    return false;
+  (void)CAN_tx_msg;
+  (void)sendMB;
+  return false;
 }
 
 //---------------------------------------------------------FLProgCanBusMessage----------------------------------------------------------
 FLProgCanBusMessage::FLProgCanBusMessage(FLProgAbstractCanBus *cunBus, uint32_t id, bool extended, uint8_t len)
 {
-    _message.id = id;
-    _message.flags.extended = extended;
-    _message.len = len;
-    _cunBus = cunBus;
+  _message.id = id;
+  _message.flags.extended = extended;
+  _message.len = len;
+  _cunBus = cunBus;
 }
 
 void FLProgCanBusMessage::send(bool value)
 {
-    if (value)
+  if (value)
+  {
+    if (!_sendOldValue)
     {
-        if (!_sendOldValue)
-        {
-            _isNeedSend = true;
-        }
+      _isNeedSend = true;
     }
-    _sendOldValue = value;
+  }
+  _sendOldValue = value;
 }
 
 uint8_t FLProgCanBusMessage::getData(uint8_t index)
 {
-    if (index > 7)
-    {
-        return 0;
-    }
-    return _message.buf[index];
+  if (index > 7)
+  {
+    return 0;
+  }
+  return _message.buf[index];
 }
 
 void FLProgCanBusMessage::setData(uint8_t index, uint8_t value)
 {
-    if (index > 7)
-    {
-        return;
-    }
-    if (_message.buf[index] == value)
-    {
-        return;
-    }
-    _message.buf[index] = value;
-    if (_mode == FLPROG_CAN_BUS_MESSAGE_PERIODICALLY_MODE)
-    {
-        return;
-    }
-    _isNeedSend = true;
+  if (index > 7)
+  {
+    return;
+  }
+  if (_message.buf[index] == value)
+  {
+    return;
+  }
+  _message.buf[index] = value;
+  if (_mode == FLPROG_CAN_BUS_MESSAGE_PERIODICALLY_MODE)
+  {
+    return;
+  }
+  _isNeedSend = true;
 }
 
 void FLProgCanBusMessage::pool()
 {
-    if ((_mode == FLPROG_CAN_BUS_MESSAGE_READ_MODE) || (_mode == FLPROG_CAN_BUS_MESSAGE_SPY_MODE))
-    {
-        readPool();
-        return;
-    }
-    sendPool();
+  if ((_mode == FLPROG_CAN_BUS_MESSAGE_READ_MODE) || (_mode == FLPROG_CAN_BUS_MESSAGE_SPY_MODE))
+  {
+    readPool();
+    return;
+  }
+  sendPool();
 }
 
 void FLProgCanBusMessage::readPool()
 {
-    _hasNewData = false;
-    if (!_cunBus->hasNewReadMessage())
+  _hasNewData = false;
+  if (!_cunBus->hasNewReadMessage())
+  {
+    return;
+  }
+  CAN_message_t *_readMessage = _cunBus->getReadMessage();
+  if (_mode == FLPROG_CAN_BUS_MESSAGE_SPY_MODE)
+  {
+    _message.flags.extended = _readMessage->flags.extended;
+    _message.id = _readMessage->id;
+  }
+  else
+  {
+    if (_readMessage->flags.extended != _message.flags.extended)
     {
-        return;
+      return;
     }
-    CAN_message_t *_readMessage = _cunBus->getReadMessage();
-    if (_mode == FLPROG_CAN_BUS_MESSAGE_SPY_MODE)
+    if (_readMessage->id != _message.id)
     {
-        _message.flags.extended = _readMessage->flags.extended;
-        _message.id = _readMessage->id;
+      return;
     }
-    else
-    {
-        if (_readMessage->flags.extended != _message.flags.extended)
-        {
-            return;
-        }
-        if (_readMessage->id != _message.id)
-        {
-            return;
-        }
-    }
-    _hasNewData = true;
-    _message.timestamp = _readMessage->timestamp;
-    _message.idhit = _readMessage->idhit;
-    _message.flags.overrun = _readMessage->flags.overrun;
-    _message.flags.reserved = _readMessage->flags.reserved;
-    _message.len = _readMessage->len;
-    _message.buf[0] = _readMessage->buf[0];
-    _message.buf[1] = _readMessage->buf[1];
-    _message.buf[2] = _readMessage->buf[2];
-    _message.buf[3] = _readMessage->buf[3];
-    _message.buf[4] = _readMessage->buf[4];
-    _message.buf[5] = _readMessage->buf[5];
-    _message.buf[6] = _readMessage->buf[6];
-    _message.buf[7] = _readMessage->buf[7];
-    _message.mb = _readMessage->mb;
-    _message.bus = _readMessage->bus;
-    _message.seq = _readMessage->seq;
+  }
+  _hasNewData = true;
+  _message.timestamp = _readMessage->timestamp;
+  _message.idhit = _readMessage->idhit;
+  _message.flags.overrun = _readMessage->flags.overrun;
+  _message.flags.reserved = _readMessage->flags.reserved;
+  _message.len = _readMessage->len;
+  _message.buf[0] = _readMessage->buf[0];
+  _message.buf[1] = _readMessage->buf[1];
+  _message.buf[2] = _readMessage->buf[2];
+  _message.buf[3] = _readMessage->buf[3];
+  _message.buf[4] = _readMessage->buf[4];
+  _message.buf[5] = _readMessage->buf[5];
+  _message.buf[6] = _readMessage->buf[6];
+  _message.buf[7] = _readMessage->buf[7];
+  _message.mb = _readMessage->mb;
+  _message.bus = _readMessage->bus;
+  _message.seq = _readMessage->seq;
 }
 
 void FLProgCanBusMessage::sendPool()
 {
-    checkNeedSend();
-    if (!_isNeedSend)
-    {
-        return;
-    }
-    _cunBus->write(_message);
-    _sendTime = millis();
-    _isNeedSend = false;
+  checkNeedSend();
+  if (!_isNeedSend)
+  {
+    return;
+  }
+  _cunBus->write(_message);
+  _sendTime = millis();
+  _isNeedSend = false;
 }
 
 void FLProgCanBusMessage::checkNeedSend()
 {
 
-    if (_mode == FLPROG_CAN_BUS_MESSAGE_CHANGE_MODE)
-    {
-        return;
-    }
-    if (_isNeedSend)
-    {
-        return;
-    }
-    if (flprog::isTimer(_sendTime, _sendPeriod))
-    {
-        _isNeedSend = true;
-    }
+  if (_mode == FLPROG_CAN_BUS_MESSAGE_CHANGE_MODE)
+  {
+    return;
+  }
+  if (_isNeedSend)
+  {
+    return;
+  }
+  if (flprog::isTimer(_sendTime, _sendPeriod))
+  {
+    _isNeedSend = true;
+  }
 }
 
 void FLProgCanBusMessage::setMode(uint8_t mode)
 {
-    if (mode > 4)
-    {
-        return;
-    }
-    _mode = mode;
+  if (mode > 4)
+  {
+    return;
+  }
+  _mode = mode;
 }
 
 void FLProgCanBusMessage::len(uint8_t value)
 {
-    if (value < 1)
-    {
-        return;
-    }
-    if (value > 8)
-    {
-        return;
-    }
-    _message.len = value;
+  if (value < 1)
+  {
+    return;
+  }
+  if (value > 8)
+  {
+    return;
+  }
+  _message.len = value;
 }
--- src/base/flprog_Can_Base.h
+++ src/base/flprog_Can_Base.h
@@ -15,29 +15,29 @@
 
 typedef struct CAN_message_t
 {
-    uint32_t id = 0;        // can identifier
-    uint16_t timestamp = 0; // time when message arrived
-    uint8_t idhit = 0;      // filter that id came from
-    struct
-    {
-        bool extended = 0; // identifier is extended (29-bit)
-        bool remote = 0;   // remote transmission request packet type
-        bool overrun = 0;  // message overrun
-        bool reserved = 0;
-    } flags;
-    uint8_t len = 8;      // length of data
-    uint8_t buf[8] = {0}; // data
-    int8_t mb = 0;        // used to identify mailbox reception
-    uint8_t bus = 1;      // used to identify where the message came (CAN1, CAN2 or CAN3)
-    bool seq = 0;         // sequential frames
+  uint32_t id = 0;        // can identifier
+  uint16_t timestamp = 0; // time when message arrived
+  uint8_t idhit = 0;      // filter that id came from
+  struct
+  {
+    bool extended = 0; // identifier is extended (29-bit)
+    bool remote = 0;   // remote transmission request packet type
+    bool overrun = 0;  // message overrun
+    bool reserved = 0;
+  } flags;
+  uint8_t len = 8;      // length of data
+  uint8_t buf[8] = {0}; // data
+  int8_t mb = 0;        // used to identify mailbox reception
+  uint8_t bus = 1;      // used to identify where the message came (CAN1, CAN2 or CAN3)
+  bool seq = 0;         // sequential frames
 } CAN_message_t;
 
 class FLProgAbstractCanBus
 {
 public:
-    virtual bool hasNewReadMessage() { return false; };
-    virtual CAN_message_t *getReadMessage() { return 0; };
-    virtual bool write(CAN_message_t &CAN_tx_msg, bool sendMB = false);
+  virtual bool hasNewReadMessage() { return false; };
+  virtual CAN_message_t *getReadMessage() { return 0; };
+  virtual bool write(CAN_message_t &CAN_tx_msg, bool sendMB = false);
 
 protected:
 };
@@ -45,36 +45,36 @@ protected:
 class FLProgCanBusMessage
 {
 public:
-    FLProgCanBusMessage(FLProgAbstractCanBus *cunBus, uint32_t id, bool extended = false, uint8_t len = 8);
-    void setMode(uint8_t mode);
-    void setSendPeriod(uint32_t sendPeriod) { _sendPeriod = sendPeriod; };
-    void send(bool value);
-    void setData(uint8_t index, uint8_t value);
-    void id(uint32_t value) { _message.id = value; };
-    void extended(bool value) { _message.flags.extended = value; };
-    void len(uint8_t value);
+  FLProgCanBusMessage(FLProgAbstractCanBus *cunBus, uint32_t id, bool extended = false, uint8_t len = 8);
+  void setMode(uint8_t mode);
+  void setSendPeriod(uint32_t sendPeriod) { _sendPeriod = sendPeriod; };
+  void send(bool value);
+  void setData(uint8_t index, uint8_t value);
+  void id(uint32_t value) { _message.id = value; };
+  void extended(bool value) { _message.flags.extended = value; };
+  void len(uint8_t value);
 
-    uint8_t getMode() { return _mode; };
-    uint32_t getSendPeriod() { return _sendPeriod; };
-    uint8_t getData(uint8_t index);
-    bool hasNewData() { return _hasNewData; };
-    CAN_message_t *message() { return &_message; };
-    uint32_t id() { return _message.id; };
-    bool extended() { return _message.flags.extended; };
-    uint8_t len() { return _message.len; };
+  uint8_t getMode() { return _mode; };
+  uint32_t getSendPeriod() { return _sendPeriod; };
+  uint8_t getData(uint8_t index);
+  bool hasNewData() { return _hasNewData; };
+  CAN_message_t *message() { return &_message; };
+  uint32_t id() { return _message.id; };
+  bool extended() { return _message.flags.extended; };
+  uint8_t len() { return _message.len; };
 
-    void pool();
+  void pool();
 
 private:
-    void readPool();
-    void sendPool();
-    void checkNeedSend();
-    CAN_message_t _message;
-    FLProgAbstractCanBus *_cunBus = 0;
-    bool _isNeedSend = true;
-    bool _hasNewData = false;
-    bool _sendOldValue = false;
-    uint8_t _mode = FLPROG_CAN_BUS_MESSAGE_READ_MODE;
-    uint32_t _sendPeriod = 50;
-    uint32_t _sendTime = flprog::timeBack(_sendPeriod);
+  void readPool();
+  void sendPool();
+  void checkNeedSend();
+  CAN_message_t _message;
+  FLProgAbstractCanBus *_cunBus = 0;
+  bool _isNeedSend = true;
+  bool _hasNewData = false;
+  bool _sendOldValue = false;
+  uint8_t _mode = FLPROG_CAN_BUS_MESSAGE_READ_MODE;
+  uint32_t _sendPeriod = 50;
+  uint32_t _sendTime = flprog::timeBack(_sendPeriod);
 };
